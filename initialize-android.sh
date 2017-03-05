aa034b21933b43cc69929fe3081baefdebad41df
Rainer Burgstaller <rainer.burgstaller@e-mundo.de>
2017-03-05 13:27:28 +0100

    fixed snap-ci build

--- initialize-android.sh
+++ initialize-android.sh
@@ -17,11 +17,11 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
 
   # The BuildTools version used by your project
   echo "installing build-tools ..."
-  echo y | android update sdk --no-ui --filter build-tools-23.0.3 --all > /dev/null
+  echo y | android update sdk --no-ui --filter build-tools-25.0.2 --all > /dev/null
 
   # The SDK version used to compile your project
   echo "installing android-23 ..."
-  echo y | android update sdk --no-ui --filter android-23 > /dev/null
+  echo y | android update sdk --no-ui --filter android-25 > /dev/null
 
   # uncomment to install the Extra/Android Support Library
   echo "installing android support ..."
